89f1909937d40072b502928d3a3159f41c4ab010
daichi <daipanchi@gmail.com>
2014-09-23 11:01:55 +0900

    Validate params

--- app/api/api.rb
+++ app/api/api.rb
@@ -6,13 +6,19 @@ class API < Grape::API
     error_response({ message: e.message })
   end
 
+  ALLOWED_PARAMS = [:beacon_id, :barcode_id, :token, :total_price, purchase: [:barcode_id, :amount]].freeze
+
   helpers do
+    def allowed_params
+      ActionController::Parameters.new(params).permit(ALLOWED_PARAMS)
+    end
+
     def beacon_id_params
-      "beacon_id=#{params[:beacon_id]}"
+      "beacon_id=#{allowed_params[:beacon_id]}"
     end
 
     def barcode_id_params
-      "barcode_id=#{params[:barcode_id]}"
+      "barcode_id=#{allowed_params[:barcode_id]}"
     end
 
     def get_from endpoint, *params
@@ -20,33 +26,51 @@ class API < Grape::API
       url = "#{endpoint}?#{params.join('&')}"
       JSON.load(http_client.get_content(url).force_encoding('utf-8'))
     end
+
+    params :require_beacon_id do requires :beacon_id, type: String end
+    params :require_barcode_id do requires :barcode_id, type: Integer end
+    params :require_webpay_token do requires :token, type: String end
+    params :require_total_price do requires :total_price, type: Integer end
+    params :require_purchase do
+      requires :purchase, type: Array do
+        requires :barcode_id
+        requires :amount
+      end
+    end
   end
 
   resource :product do
+    params { use :require_beacon_id, :require_barcode_id }
     get do
       get_from ENV['PRODUCT_ENDPOINT'], beacon_id_params, barcode_id_params
     end
   end
 
   resource :products do
+    params { use :require_beacon_id }
     get do
       get_from ENV['PRODUCTS_ENDPOINT'], beacon_id_params
     end
   end
 
   resource :newspapers do
+    params { use :require_beacon_id }
     get do
       get_from ENV['NEWSPAPERS_ENDPOINT'], beacon_id_params
     end
   end
 
   resource :purchase do
+    params do
+      use :require_beacon_id, :require_webpay_token, :require_purchase, :require_total_price
+    end
+
     post do
-      @purchase = Purchase.create( beacon_id: params[:beacon_id],
-                                   total_price: params[:total_price], )
-      if responce_from_pos = (@purchase.purchase_post_to_pos params)
-        unless responce_from_webpay = (@purchase.webpay_with params[:token])
-          @purchase.cancel_purchase_post_to_pos params
+      @purchase = Purchase.create( beacon_id: allowed_params[:beacon_id],
+                                   total_price: allowed_params[:total_price], )
+      if responce_from_pos = (@purchase.purchase_post_to_pos allowed_params)
+        unless responce_from_webpay = (@purchase.webpay_with allowed_params[:token])
+          @purchase.cancel_purchase_post_to_pos allowed_params
         end
         { responce_from_pos: responce_from_pos,
           responce_from_webpay: responce_from_webpay }
---
+++ spec/requests/api/purchase_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+describe API do
+  include Rack::Test::Methods
+
+  describe 'Purchase' do
+    let!(:without_params) { {} }
+
+    context 'purchase products' do
+      let(:response) do
+        post '/api/v0/purchase', without_params
+      end
+
+      before { @error_sentences = JSON.load(response.body)['error'].split(', ') }
+
+      it 'returns status 500' do
+        expect(response.status).to eq(500)
+      end
+
+      it 'returns the error of missing beacon_id' do
+        expect(@error_sentences).to include('beacon_id is missing')
+      end
+
+      it 'returns the error of missing token' do
+        expect(@error_sentences).to include('token is missing')
+      end
+
+      it 'returns the error of missing purchase' do
+        expect(@error_sentences).to include('purchase is missing')
+      end
+
+      it 'returns the error of missing purchase' do
+        expect(@error_sentences).to include('total_price is missing')
+      end
+    end
+  end
+end
